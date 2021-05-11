31f2a5609f968e5f401fc00226373e6af911955b
ribeirogab <gblosr@gmail.com>
2021-05-11 10:35:53 -0300

    feat: sign in and dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-export default function Home() {
+import { Flex, Button, Stack } from '@chakra-ui/react';
+
+import { Input } from '../components/Form'
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
-  )
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input label="E-mail" name="email" type="email" />
+          <Input label="Password" name="password" type="password" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
+  );
 }
