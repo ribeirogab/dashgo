89a1846de5e6e521a5fb8fd2c35eea0522ef595e
ribeirogab <gblosr@gmail.com>
2021-05-12 07:50:10 -0300

    feat: user control, list and create user pages

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import { Box, Stack, Text, Icon, Link } from '@chakra-ui/react';
+import Link from 'next/link'
+import { Box, Stack, Text, Icon, Link as ChakraLink } from '@chakra-ui/react';
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from 'react-icons/ri';
 
 export function Sidebar() {
@@ -8,13 +9,18 @@ export function Sidebar() {
         <Box>
           <Text fontWeight="bold" color="gray.400" fontSize="small">GERAL</Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
-              <Icon as={RiDashboardLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">Dashboard</Text>
+            <Link href="/dashboard">
+              <ChakraLink display="flex" align="center">
+                <Icon as={RiDashboardLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Dashboard</Text>
+              </ChakraLink>
             </Link>
-            <Link display="flex" align="center">
-              <Icon as={RiContactsLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">Usuários</Text>
+
+            <Link href="/users">
+              <ChakraLink display="flex" align="center">
+                <Icon as={RiContactsLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Usuários</Text>
+              </ChakraLink>
             </Link>
           </Stack>
         </Box>
@@ -24,13 +30,18 @@ export function Sidebar() {
             AUTOMAÇÃO
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
-              <Icon as={RiInputMethodLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">Formulários</Text>
+            <Link href="#">
+              <ChakraLink display="flex" align="center">
+                <Icon as={RiInputMethodLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Formulários</Text>
+              </ChakraLink>
             </Link>
-            <Link display="flex" align="center">
-              <Icon as={RiGitMergeLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">Automação</Text>
+
+            <Link href="#">
+              <ChakraLink display="flex" align="center">
+                <Icon as={RiGitMergeLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Automação</Text>
+              </ChakraLink>
             </Link>
           </Stack>
         </Box>
